79dcc7825990db05a9869514e78d2aec63223994
martinafarah6 <martinafarah6@gmail.com>
2026-09-19 15:14:35 +0300

    Implement footer component with social links
    Added social media links and copyright notice to the footer.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+export default function Footer()
+{
+    return(
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="https://www.facebook.com/?utm_source=chatgpt.com" target="_blank" rel="noreferrer"><img src="https://i.pinimg.com/736x/1c/52/8e/1c528e2785837c261958427640c9e846.jpg" alt="facebook" width="30px"/></a></li>
+                    <li><a href="https://www.whatsapp.com/?utm_source=chatgpt.com" target="_blank" rel="noreferrer"><img src="https://i.pinimg.com/1200x/26/7d/66/267d6683af970399e843bd4c87435f32.jpg" alt="whatsapp" width="30px"/></a></li>
+                    <li><a href="https://www.instagram.com/?utm_source=chatgpt.com" target="_blank" rel="noreferrer"><img src="https://i.pinimg.com/1200x/89/9c/96/899c9681a1985c543b313628f4ed8592.jpg" alt="insta" width="30px"/></a></li>
+                    <li><a href="https://www.linkedin.com/?utm_source=chatgpt.com" target="_blank" rel="noreferrer"><img src="https://i.pinimg.com/736x/44/28/c1/4428c1f269ea89979de6ba98709f0548.jpg" alt="linkedin" width="30px"/></a></li>
+                </ul>
+            </nav>
+            <p>@E_Commerce. All rights reserved.</p>
+        </footer>
+    )
+}
+
+
+
